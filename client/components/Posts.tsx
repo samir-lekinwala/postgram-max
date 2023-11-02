e87dd2369a56f89ab69900d2d74209b739cb85a4
Cong <53280539+Jero327@users.noreply.github.com>
2023-11-03 11:49:46 +1300

    Posts page

--- client/components/Posts.tsx
+++ client/components/Posts.tsx
@@ -2,16 +2,80 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Posts() {
-  const [liked, setLiked] = useState(false)
-
-  function handleLike() {
-    setLiked(!liked)
-  }
+  const [posts, setPosts] = useState([
+    {
+      owner: 'Cong',
+      description: 'Lazy cat',
+      liked: false,
+      img: 'https://www.thesprucepets.com/thmb/APYdMl_MTqwODmH4dDqaY5q0UoE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg',
+    },
+    {
+      owner: 'Samir',
+      description: 'Lazy cat',
+      liked: false,
+      img: 'https://www.thesprucepets.com/thmb/APYdMl_MTqwODmH4dDqaY5q0UoE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg',
+    },
+    {
+      owner: 'V',
+      description: 'Lazy cat',
+      liked: false,
+      img: 'https://www.thesprucepets.com/thmb/APYdMl_MTqwODmH4dDqaY5q0UoE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg',
+    },
+    {
+      owner: 'Ricky',
+      description: 'Lazy cat',
+      liked: false,
+      img: 'https://www.thesprucepets.com/thmb/APYdMl_MTqwODmH4dDqaY5q0UoE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg',
+    },
+  ])
 
   return (
-    <div>
+    <div className="posts">
       <Link to={`/add`}>Add</Link>
-      <button onClick={handleLike} style={{ fontSize: '24px', color: liked ? 'red' : 'black' }}>{liked? '❤️' : '♡'}</button>
+      <ul>
+        {posts.map((post) => (
+          <button
+            className="post"
+            key={post.img}
+            onDoubleClick={() => {
+              setPosts(
+                posts.map((item) => {
+                  if (item !== post) {
+                    return item
+                  } else {
+                    return { ...post, liked: !post.liked }
+                  }
+                })
+              )
+            }}
+          >
+            <li>
+              <p>Owner: {post.owner}</p>
+              <img alt={post.description} src={post.img} />
+              <div className="descriptionLiked-container">
+                <p className="description">{post.description}</p>
+                <button
+                  className="liked"
+                  onClick={() => {
+                    setPosts(
+                      posts.map((item) => {
+                        if (item !== post) {
+                          return item
+                        } else {
+                          return { ...post, liked: !post.liked }
+                        }
+                      })
+                    )
+                  }}
+                  style={{ fontSize: '24px' }}
+                >
+                  {post.liked ? '❤️' : '♡'}
+                </button>
+              </div>
+            </li>
+          </button>
+        ))}
+      </ul>
     </div>
   )
 }
