9fd06e750a3e6b9684f150ee7d47710113fb5596
Samir Lekinwala <samirlekinwala@gmail.com>
2023-11-03 10:05:23 +1300

    Add images to images.tsx

---
+++ client/images.tsx
@@ -0,0 +1,17 @@
+export const images = {
+  fewStudents: '20231102_084724.jpg',
+  vitor: '20231102_085256.jpg',
+  ricky: '20231102_085322.jpg',
+  gapson: '20231102_085350.jpg',
+  daph: '20231102_161940.jpg',
+  cong: '20231102_162013.jpg',
+  jared: '20231102_162039.jpg',
+  xavier: '20231102_162116.jpg',
+  team1: '20231102_162414.jpg',
+  richXavier: '20231102_162625.jpg',
+  bell: '20231102_162838.jpg',
+  gangster: '20231102_163424.jpg',
+  will: 'IMG_4351.jpg',
+}
+
+export default images
