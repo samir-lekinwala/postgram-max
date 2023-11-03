15cf18c9eac24402897a79c76cfe1b7f8302e541
Samir Lekinwala <samirlekinwala@gmail.com>
2023-11-03 14:34:24 +1300

    Update form to clear

--- client/components/Posts.tsx
+++ client/components/Posts.tsx
@@ -32,11 +32,57 @@ function Posts() {
       liked: false,
       img: 'https://www.thesprucepets.com/thmb/APYdMl_MTqwODmH4dDqaY5q0UoE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg',
     },
+    {
+      owner: 'Will',
+      description: 'On the run from the police',
+      liked: true,
+      img: 'client/public/images/IMG_4351.jpg',
+    },
+    {
+      owner: 'Samir',
+      description: 'Empty class',
+      liked: false,
+      img: 'client/public/images/20231102_084724.jpg',
+    },
+    {
+      owner: 'Samir',
+      description: 'Working hard or hardly working?',
+      liked: true,
+      img: 'client/public/images/20231102_162116.jpg',
+    },
+    {
+      owner: 'Samir',
+      description: 'Hug it out bois',
+      liked: true,
+      img: 'client/public/images/20231102_162838.jpg',
+    },
+    {
+      owner: 'Samir',
+      description: 'Straight up gangsta',
+      liked: false,
+      img: 'client/public/images/20231102_163424.jpg',
+    },
+    {
+      owner: 'Samir',
+      description: 'Sleepy boi',
+      liked: false,
+      img: 'client/public/images/20231102_162039.jpg',
+    },
+    {
+      owner: 'Samir',
+      description: 'Still waiting on my money Rich',
+      liked: true,
+      img: 'client/public/images/20231102_162625.jpg',
+    },
   ])
 
   const [post, setPost] = useState({ name: '', image: '', caption: '' })
   // const [name, setName] = useState('')
   const [imageSelected, setImageSelected] = useState('')
+
+  const onClear = () => {
+    setValue('')
+  }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -48,6 +94,8 @@ function Posts() {
     console.log(image)
     console.log(caption)
     console.log(name)
+    setPost({ name: '', image: '', caption: '' })
+    setImageSelected('')
     // setImageSelected(image as string)
     // setPost({ name: name, image: image, caption: caption } as postData)
     setPosts([
@@ -67,12 +115,18 @@ function Posts() {
         <form onSubmit={handleSubmit} method="post" action="/form">
           <label htmlFor="name">Name: </label>
           <input
+            required
             id="name"
             name="name"
             placeholder="Your name to be displayed"
+            value={post.name}
+            onChange={(e) =>
+              setPost({ name: e.target.value, image: '', caption: '' })
+            }
           ></input>
           <label htmlFor="dropdown">Select an Image</label>
           <select
+            required
             name="dropdown"
             id="dropdown"
             onChange={(event) => {
@@ -84,15 +138,25 @@ function Posts() {
           </select>
           <label htmlFor="caption">Caption</label>
           <input
+            required
             type="text"
             name="caption"
             id="caption"
             placeholder="Enter a caption here!"
+            value={post.caption}
+            onChange={(e) =>
+              setPost({
+                // name: e.target.value,
+                image: '',
+                caption: e.target.value,
+              })
+            }
           ></input>
           <button type="submit" onClick={() => handleSubmit}>
             Submit
           </button>
         </form>
+        <br />
         {imageSelected && (
           <img
             width="400px"
